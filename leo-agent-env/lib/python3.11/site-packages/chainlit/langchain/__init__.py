0b763b6852fe25aed950d0a77ba4b43ab17b290c
Caleb Seely <calebseely@Calebs-Mac-Studio.local>
2026-05-20 14:38:49 -0400

    Clean repo and add gitignore

--- leo-agent-env/lib/python3.11/site-packages/chainlit/langchain/__init__.py
+++
@@ -1,6 +0,0 @@
-from chainlit.utils import check_module_version
-
-if not check_module_version("langchain_core", "0.2.5"):
-    raise ValueError(
-        "Expected langchain-core version >= 0.2.5. Run `pip install langchain-core --upgrade`"
-    )
